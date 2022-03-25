c4482d58dfee107e2d52337189fe7e15fe8bc69c
gembis <94685136+gembis@users.noreply.github.com>
2022-03-25 12:37:21 +0100

    Add files via upload

---
+++ JS/navbar.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "10px";
+  document.getElementById("main").style.marginTop= "10px";
+}
