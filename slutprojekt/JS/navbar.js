003ca1f45988a0951ec0f9e6bbf0f6cd6af8920e
gembis <94685136+gembis@users.noreply.github.com>
2022-04-08 11:44:50 +0200

    Add files via upload

--- slutprojekt/JS/navbar.js
+++ slutprojekt/JS/navbar.js
@@ -16,31 +16,3 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "10px";
   document.getElementById("main").style.marginTop= "0";
 }
-
-/*
------------------------------------------------
-var x = window.matchMedia("(max-width: 768px)")
-openNavbar(x)
-x.addListener(openNavbar)
-
-function openNavbar(x) {
-  if (x.matches) {
-    function responsiveNavbar() {
-      var element = document.getElementById("mySidenav", "mySidenavBG");
-      element.classList.add("open");
-    }
-  }
-}
-
-
-
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width ="0";
-  document.getElementById("mySidenav").style.border = "0px solid #8f6442";
-  document.getElementById("mySidenavBG").style.width = "0";
-  document.getElementById("mySidenavBG").style.transition = ".57s";
-  document.getElementById("main").style.marginLeft= "10px";
-  document.getElementById("main").style.marginTop= "0";
-}
-*/
